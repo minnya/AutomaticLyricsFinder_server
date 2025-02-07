13d42ace623ec0638fdb5b167a3de450d00d2858
minnya <minnya9018@yahoo.co.jp>
2025-02-08 05:42:50 +0900

    セッションのリサイクルを追加

--- app/database/manager.py
+++ app/database/manager.py
@@ -8,6 +8,7 @@ engine = create_engine(
     f"mysql+pymysql://{os.getenv('MARIADB_USER')}:{os.getenv('MARIADB_PASSWORD')}@"
     f"{os.getenv('MARIADB_HOST')}/{os.getenv('MARIADB_DATABASE')}",
     echo=False,  # ログ出力を有効にする
+    pool_recycle=3600,  # 1時間ごとに接続をリサイクル
 )
 # Sessionの作成
 session = scoped_session(sessionmaker(autocommit=False, autoflush=False, bind=engine))
